e3350913375e09e18b57ad60c57f9f0b950e090c
Scott <scott.crisp@oneadvanced.com>
2023-09-12 15:59:21 +0100

    feat: Added unit test suite for auth app

--- django_notes/authentication/tests.py
+++ django_notes/authentication/tests.py
@@ -1,3 +1,53 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 
-# Create your tests here.
+from . tokens import generate_token
+
+class AuthenticationViewsTestCase(TestCase):
+    def setUp(self):
+        self.user_data = {
+            'username': 'testuser',
+            'firstname': 'Test',
+            'lastname': 'User',
+            'email': 'test@example.com',
+            'password': 'testpassword',
+            'verifypassword': 'testpassword',
+        }
+        
+    def test_signup_view(self):
+        response = self.client.get(reverse('signup')) 
+        self.assertEqual(response.status_code, 200)
+        
+        response = self.client.post(reverse('signup'), data=self.user_data)
+        self.assertEqual(response.status_code, 302)
+        
+        self.assertTrue(User.objects.filter(username=self.user_data['username']).exists())
+        user = User.objects.get(username=self.user_data['username'])
+        self.assertFalse(user.is_active)
+        
+    def test_signin_view(self):
+        User.objects.create_user(username=self.user_data['username'], password=self.user_data['password'])
+        
+        response = self.client.get(reverse('signin'))
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.post(reverse('signin'), data={'username': 'testuser', 'password': 'testpassword'})
+        self.assertEqual(response.status_code, 302)
+
+        self.assertIn('_auth_user_id', self.client.session)
+        
+    def test_activate_view(self):
+        user = User.objects.create_user(username=self.user_data['username'], password=self.user_data['password'])
+        
+        uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
+        token = generate_token.make_token(user)
+        activation_url = reverse('activate', args=[uidb64, token])
+
+        response = self.client.get(activation_url)
+        self.assertEqual(response.status_code, 302) 
+
+        user = User.objects.get(username=self.user_data['username'])
+        self.assertTrue(user.is_active)
--- django_notes/authentication/views.py
+++ django_notes/authentication/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import redirect, render
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import EmailMessage
+from django.shortcuts import redirect, render
 from django.template.loader import render_to_string
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 
 from . tokens import generate_token
 from django_notes import settings
